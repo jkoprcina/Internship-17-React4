b8d0fdd7bbff33ad943fb28f3269f77abc7e0a96
jkoprcina <koprcina.josip@gmail.com>
2019-05-23 12:49:05 +0200

    Created a single hex, learned rules and wrote down components. Trying to learn redux before continuing

---
+++ catan/src/Components/board.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hex from "./hex.js";
+
+class Board extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hex />
+      </div>
+    );
+  }
+}
+
+export default Board;
---
+++ catan/src/Components/hex.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ".././Css/hex.css";
+
+class Hex extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="hex-top" />
+        <div className="hex-middle" />
+        <div className="hex-bottom" />
+      </>
+    );
+  }
+}
+
+export default Hex;
